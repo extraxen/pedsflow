326ca0597c646c9229074ab23a97acddabe4f22f
extraxen <extraxen@gmail.com>
2026-09-04 22:55:31 -0400

    Release PedsFlow 20.6.0 cardiology update

--- lib/app_metadata.dart
+++ lib/app_metadata.dart
@@ -2,9 +2,9 @@
 // Copyright (c) 2026 Ahmed Saleh. All rights reserved.
 // See LICENSE in the repository root.
 
-const String pedsFlowVersion = '20.5.1';
-const int pedsFlowBuildNumber = 2051;
-const String pedsFlowVersionLabel = '20.5.1+2051';
-const String pedsFlowReleaseDate = 'August 29, 2026';
+const String pedsFlowVersion = '20.6.0';
+const int pedsFlowBuildNumber = 2060;
+const String pedsFlowVersionLabel = '20.6.0+2060';
+const String pedsFlowReleaseDate = 'September 4, 2026';
 
 
