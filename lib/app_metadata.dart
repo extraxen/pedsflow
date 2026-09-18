437c268b36c7960581345268540d2c0edaab4465
extraxen <extraxen@gmail.com>
2026-09-18 12:38:45 -0400

    Bump PedsFlow to 20.10.3

--- lib/app_metadata.dart
+++ lib/app_metadata.dart
@@ -2,7 +2,7 @@
 // Copyright (c) 2026 Ahmed Saleh. All rights reserved.
 // See LICENSE in the repository root.
 
-const String pedsFlowVersion = '20.10.2';
-const int pedsFlowBuildNumber = 2102;
+const String pedsFlowVersion = '20.10.3';
+const int pedsFlowBuildNumber = 2103;
 const String pedsFlowVersionLabel = '20.10.2+2102';
-const String pedsFlowReleaseDate = 'September 12, 2026';
+const String pedsFlowReleaseDate = 'September 18, 2026';
